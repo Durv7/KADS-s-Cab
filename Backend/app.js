4256ab6dbc83c3c412ff64ca00c8ebcb5f5a1a23
Durvankur <durvankurkumbhar43@gmail.com>
2024-11-21 11:39:25 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -29,7 +29,7 @@ app.use(cors({
 }));
 const io = socketIo(server, {
     cors:{
-        origin:url,
+        origin:[`${process.env.FRONTEND_URL}`,'http://localhost:5173'],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     }
