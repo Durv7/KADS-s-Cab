769d8bfb0d2ac6bacc51340cca3f71503e089356
Durvankur <durvankurkumbhar43@gmail.com>
2024-11-21 11:52:17 +0530

    CORS-update

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: url,
+    origin: [`${process.env.FRONTEND_URL}`,'http://localhost:5173'],
     credentials: true,
 }));
 const io = socketIo(server, {
