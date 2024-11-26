d4136d8c809724adb033bc641acb684642da3037
Durvankur <durvankurkumbhar43@gmail.com>
2024-11-27 00:59:42 +0530

    sameSite-update

--- Backend/Controllers/authCustomer.controllers.js
+++ Backend/Controllers/authCustomer.controllers.js
@@ -16,7 +16,7 @@ const setToken = (res,token)=>{
     res.cookie('kads_token', token, {
         httpOnly: false,
         secure: process.env.NODE_ENV === 'production', // Use secure flag in production
-        sameSite:'none',
+        sameSite:'strict',
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       });
 }
